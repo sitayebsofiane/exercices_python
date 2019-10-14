5e92f55dcc9d4cdc13d91e76ade4d46b495f5804
sitayebsofiane <sitayebsofiane51@gmail.com>
2019-10-14 19:36:17 +0200

    display result of arithmitiques operations

--- partie1.py
+++ partie1.py
@@ -1,7 +1,15 @@
 #coding:utf8
 
+#exo1:
 #display "hello world" in terminal
 print("Hello world")
 #display "hello world" in terminal with variable
 bruno="Hello world"
 print(bruno)
+#exo2:
+#display result of arithmitiques operations
+print("res_mulication:",3*3,"res_addition:",4+9+78,"res_substraction:",12-7,"res_power:",5e4)
+try:
+    print("division:",12/0)
+except ZeroDivisionError:
+    print("impossible de deviser par zero")
