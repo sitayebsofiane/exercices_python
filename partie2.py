276e757b33920c32b89d7c88da581dfb22fbacdb
sitayebsofiane <sitayebsofiane51@gmail.com>
2019-10-16 16:09:59 +0200

    swith for 5 exos

--- partie2.py
+++ partie2.py
@@ -1,51 +1,70 @@
-#coding:utf8
+def alter():
+    option=input("quel exo vous voulez taper exemple:exo1 pour le premier :")
+    def exo1():
+        var1 = ""
+        var2 = ""
 
-#exo1:
-var1=""
-var2=""
-#cheking var1,var2 if empty
-def verifVar(*var):
-    res=""
-    for el in var:
-        res +=el
-    return res==""
-print(verifVar(var1,var2))
-#exo2:
-import time
-try:
-    actuel_year=int(input("entrez l'année actuel: "))
-    if(actuel_year != int(time.strftime("%Y"))):
-        print("l'année actuel est :",time.strftime("%Y"))
-    actuel_year=int(time.strftime("%Y"))
-    death_year=int(input("entrez votre année de naissaice: "))
-    age=actuel_year-death_year
-    print("votre age est ",age)
-    print("le cumul des age: ",age+int(input("age de la personne a coté: ")))
-except ValueError:
-    time.sleep(1)
-    print("vous avez mal resigner les année")
-#exo3
+        # cheking var1,var2 if empty
+        def verifVar(*var):
+            res = ""
+            for el in var:
+                res += el
+            return res == ""
 
-prix1=70
-prix2=59
-prix3=20
-total= lambda total:total-total*20/100
-print("total est : ",total(prix1+prix2+prix3))
+        print(verifVar(var1, var2))
 
-#exo4
 
-print("resultat",float(input("entrez le premier nombre a aditionner "))
-                +
-float(input("entrez le deuxieme nombre a aditionner ")))
+    # exo2:
+    def exo2():
+        import time
+        try:
+            actuel_year = int(input("entrez l'année actuel: "))
+            if (actuel_year != int(time.strftime("%Y"))):
+                print("l'année actuel est :", time.strftime("%Y"))
+            actuel_year = int(time.strftime("%Y"))
+            death_year = int(input("entrez votre année de naissaice: "))
+            age = actuel_year - death_year
+            print("votre age est ", age)
+            print("le cumul des age: ", age + int(input("age de la personne a coté: ")))
+        except ValueError:
+            time.sleep(1)
+            print("vous avez mal resigner les année")
 
-#exo5
-import math
-prenom=input("entrez le prenom ").upper()
-nom=input("entrez le nom ").upper()
-#affiche la premiere et la derniere lettre de son prenom en majuscule
-print(prenom[0],prenom[-1])
-#affiche la premiere et la derniere lettre de son nom en majuscule
-print(nom[0],nom[-1])
-print(prenom[0]+prenom[-1]+nom[0]+nom[-1])
-print("l'age divisé par 33 et arrondi entier sup ",math.ceil(float(input("entrez votre age "))/33))
+
+    # exo3
+    def exo3():
+        prix1 = 70
+        prix2 = 59
+        prix3 = 20
+        total = lambda total: total - total * 20 / 100
+        print("total est : ", total(prix1 + prix2 + prix3))
+
+
+    # exo4
+    def exo4():
+        print("resultat", float(input("entrez le premier nombre a aditionner "))
+              +
+              float(input("entrez le deuxieme nombre a aditionner ")))
+
+
+    # exo5
+    def exo5():
+        import math
+        prenom = input("entrez le prenom ").upper()
+        nom = input("entrez le nom ").upper()
+        # affiche la premiere et la derniere lettre de son prenom en majuscule
+        print(prenom[0], prenom[-1])
+        # affiche la premiere et la derniere lettre de son nom en majuscule
+        print(nom[0], nom[-1])
+        print(prenom[0] + prenom[-1] + nom[0] + nom[-1])
+        print("l'age divisé par 33 et arrondi entier sup ", math.ceil(float(input("entrez votre age ")) / 33))
+
+
+    dico = {"exo1": exo1, "exo2": exo2, "exo3": exo3, "exo4": exo4, "exo5": exo5}
+    try:
+        dico.get(option)()
+    except:
+        print("entrez exo1,exo2....")
+alter()
+
 
